417c363c9f4e3bcfab5938406099580b111e5cff
Carlos Alejandro Hernández Mejía <JandroMejia97@users.noreply.github.com>
2018-10-21 12:09:55 -0600

    Corriendo error
    Se agregó la carpeta apps al repositorio

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\aleja\onedrive\documents\ciclo vi - 2018\arc115\proyecto\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
