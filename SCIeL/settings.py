257a046f5e51f9d2a49bafbcc4ce25356f198147
Carlos Alejandro Hernández Mejía <JandroMejia97@users.noreply.github.com>
2018-10-23 00:25:48 -0600

    Error

--- SCIeL/settings.py
+++ SCIeL/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SCIeL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scieldb',
+        'USER': 'sciel',
+        'PASSWORD': 'Admin123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -114,8 +118,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
