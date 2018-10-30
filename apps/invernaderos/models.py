3383749d1c4a9c058198b8df0cb3cafa9e9f8bd2
EliMendez <elixa_mendex@hotmail.com>
2018-10-29 22:17:27 -0600

    Modificando models

--- apps/invernaderos/models.py
+++ apps/invernaderos/models.py
@@ -1,26 +1,9 @@
 from django.db import models
 
-
-class Actuador(models.Model):
-    nombre = models.CharField(
-        max_length=45
-    )
-    activado = models.BooleanField(
-        verbose_name='Activado',
-        name='Activado',
-        null=False
-    )
-
-    def __str___(self):
-        return self.nombre
-
-    class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Actuador"
-        verbose_name_plural = "Actuadores"
-
-
 class Cultivo(models.Model):
+    idCultivo = models.Autofield(
+        primary_key=True
+    )
     nombre = models.CharField(
         max_length=45
     )
@@ -38,6 +21,9 @@ class Cultivo(models.Model):
 
 
 class Etapa(models.Model):
+    idEtapa = models.Autofield(
+        primary_key=True
+    )
     numero = models.IntegerField(
         verbose_name='Número',
         blank=False
@@ -52,7 +38,7 @@ class Etapa(models.Model):
     descripcion = models.CharField(
         max_length=150
     )
-    cultivo = models.ForeignKey(
+    idCultivo = models.ForeignKey(
         Cultivo,
         on_delete=models.CASCADE
     )
@@ -67,26 +53,124 @@ class Etapa(models.Model):
 
 
 class Parametro(models.Model):
+    idParametro = models.Autofield(
+        primary_key=True
+    )
     nombre = models.CharField(
         max_length=45
+    )
+    idCultivo = models.ForeignKey(
+        Cultivo,
+        on_delete=models.CASCADE
     )
 
     def __str__(self):
         return self.nombre
 
     class Meta:
-        ordering = ['nombre']
-        verbose_name = 'Parámetro'
-        verbose_name_plural = 'Parámetros'
+        ordering = ["cultivo","nombre"]
+        verbose_name = "Parámetro"
+        verbose_name_plural = "Parámetros"
 
 
-class Sensor(models.Model):
+class Invernadero(models.Model):
+    idInvernadero = models.Autofield(
+        primary_key=True
+    )
     nombre = models.CharField(
         max_length=45
     )
+    ubicacion = models.CharField(
+        max_length=15
+    )
+    idCultivo = models.ForeignKey(
+        Cultivo,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["cultivo", "nombre"]
+        verbose_name = "Invernadero"
+        verbose_name_plural = "Invernaderos"
+
+
+class Dispositivo(models.Model):
+    idDispositivo = models.Autofield(
+        primary_key=True
+    )
+    nombre = models.CharField(
+        max_length=45
+    )
+    estado = models.CharField(
+        max_length=45
+    )
+    idInvernadero = models.ForeignKey(
+        Invernadero,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["invernadero", "nombre", "estado"]
+        verbose_name = "Dispositivo"
+        verbose_name_plural = "Dispositivos"
+
+class Actuador(models.Model):
+    idActuador = models.Autofield(
+        primary_key=True
+    )
+    nombre = models.CharField(
+        max_length=45
+    )
+    activado = models.BooleanField(
+        verbose_name='Activado',
+        name='Activado',
+        null=False
+    )
+    idDispositivo = models.ForeignKey(
+        Dispositivo,
+        on_delete=models.CASCADE
+    )
+
+    def __str___(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["dispositivo","nombre"]
+        verbose_name = "Actuador"
+        verbose_name_plural = "Actuadores"
+
+
+class Sensor(models.Model):
+    idSensor = models.Autofield(
+        primary_key=True
+    )
+    nombre = models.CharField(
+        max_length=45
+    )
+    idDispositivo = models.ForeignKey(
+        Dispositivo,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["dispositivo", "nombre"]
+        verbose_name = "Sensor"
+        verbose_name_plural = "Sensores"
 
 
 class Medicion(models.Model):
+    idMedicion = models.Autofield(
+        primary_key=True
+    )
     magnitud = models.DecimalField(
         verbose_name='Magnitud',
         decimal_places=2,
@@ -101,8 +185,27 @@ class Medicion(models.Model):
         verbose_name='Fecha de lectura',
         auto_now_add=True
     )
+    idCultivo = models.ForeignKey(
+        Cultivo,
+        on_delete=models.CASCADE
+    )
+    idParametro = models.ForeignKey(
+        Parametro,
+        on_delete=models.CASCADE
+    )
+    idSensor = models.ForeignKey(
+        Sensor,
+        on_delete=models.CASCADE
+    )
+    idActuador = models.ForeignKey(
+        Actuador,
+        on_delete=models.CASCADE
+    )
 
+    def __str__(self):
+        return self.nombre
 
-class Invernadero(models.Model):
-    nombre = models.CharField(max_length=45)
-    ubicacion = models.CharField(max_length=15)
+    class Meta:
+        ordering = ["cultivo", "sensor", "parametro", "actuador", "numero", "nombre"]
+        verbose_name = "Medicion"
+        verbose_name_plural = "Mediciones"
