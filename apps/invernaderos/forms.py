489bbe9e85d4828109beeca32dd7dfe0cd8eb761
Carlos Alejandro Hernández Mejía <alejhrndz97@outlook.com>
2018-10-30 21:15:58 -0600

    Commit de emergencia para recuperar informacion

--- apps/invernaderos/forms.py
+++ apps/invernaderos/forms.py
@@ -1,27 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
-
-
-class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(
-        max_length=40, 
-        required=True
-    )
-    last_name = forms.CharField(max_length=140, required=False)
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = (
-            'username',
-            'email',
-            'first_name',
-            'last_name',
-            'password1',
-            'password2',
-        )
-
 
 
 class LoginForm(AuthenticationForm):
@@ -52,5 +31,6 @@ class LoginForm(AuthenticationForm):
             'password1',
             'password2',
         )
+        
         
     
