7a7130be7990371b0abc87a11527650be2c960e5
RosaAlfaro <rosalfaro96@gmail.com>
2018-10-30 21:40:56 -0600

    admin.py

--- apps/invernaderos/admin.py
+++ apps/invernaderos/admin.py
@@ -4,15 +4,3 @@ from .models import *
 admin.site.register(Cultivo)
 
 admin.site.register(Invernadero)
-
-admin.site.register(Etapa)
-
-admin.site.register(Dispositivo)
-
-admin.site.register(Sensor)
-
-admin.site.register(Actuador)
-
-admin.site.register(Medicion)
-
-admin.site.register(Parametro)
